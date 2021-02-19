8aeea789d53ed8cf1f3cd08b6875dc5454219987
aliselmes <a.selmes@gmail.com>
2021-02-18 20:35:07 -0600

    gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '425788198631598',
-        clientSecret: 'e9c44f5915d8aec7e63c66f480140327'
-    }
-}
